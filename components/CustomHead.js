5d75c10e18d89aa9e74645842137fdaec1c12dd1
Alex Parrott <alex.parrott@quadrotech-it.com>
2020-06-09 22:19:56 +0100

    Title and description fixed

--- components/CustomHead.js
+++ components/CustomHead.js
@@ -2,17 +2,12 @@ import Head from 'next/head';
 
 const CustomHead = () => (
   <Head>
-    <title>{'<Alex Parrott.js>'}</title>
+    <title>Alex Parrott | Web Developer</title>
     <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    <meta name="Description" CONTENT="Alex Parrott is a full stack web developer based in Bristol, UK" />
     <link rel="icon" href="/favicon.ico" />
-    <link
-      href="https://fonts.googleapis.com/css?family=Lexend+Tera:300,400,700,800"
-      rel="stylesheet"
-    />
-    <link
-      href="https://fonts.googleapis.com/css?family=Montserrat:300,400,700,800"
-      rel="stylesheet"
-    />
+    <link href="https://fonts.googleapis.com/css?family=Lexend+Tera:300,400,700,800" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,700,800" rel="stylesheet" />
   </Head>
 );
 
