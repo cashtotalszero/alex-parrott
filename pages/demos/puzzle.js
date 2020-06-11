0eace83c9e6070aba058586dc821a0a2e963ec77
Alex Parrott <alex.parrott@quadrotech-it.com>
2020-06-11 19:01:57 +0100

    Unused images removed

--- pages/demos/puzzle.js
+++ pages/demos/puzzle.js
@@ -1,5 +1,4 @@
 import { DndProvider } from 'react-dnd';
-import { HTML5Backend } from 'react-dnd-html5-backend';
 import { TouchBackend } from 'react-dnd-touch-backend';
 
 import Page from '../../components/Page';
@@ -8,7 +7,6 @@ import CustomDragLayer from '../../components/PuzzleDragLayer';
 
 const PuzzlePage = () => (
   <Page>
-    {/* <DndProvider backend={HTML5Backend}> */}
     <DndProvider backend={TouchBackend} options={{ enableMouseEvents: true, enableTouchEvents: true }}>
       <Container />
       <CustomDragLayer />
